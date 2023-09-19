8713cefe9ac5279a6410ac9144706985b4ff003f
MenassieErmias <ermiasmenase@gmail.com>
2023-09-19 15:54:45 +0300

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='w-full bg-black/10 text-white py-4 mt-4'>
+            <p className='text-center text-2xl'>Done by Menassie Ermias</p>
+        </div>
+    )
+}
+
+export default Footer
